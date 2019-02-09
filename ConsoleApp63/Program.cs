ab6d96b0102c25401a9162ba5f2da9ae5326e14d
AronThomasMiller <AronThomasMiller@gmail.com>
2019-02-09 19:24:08 +0200

    first commit

---
+++ ConsoleApp63/Program.cs
@@ -0,0 +1,21 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace ConsoleApp63
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter text to Google");
+            var word = Console.ReadLine();
+            IWebDriver driver = new ChromeDriver();
+            driver.Navigate().GoToUrl("http://google.com");
+            driver.FindElement(By.XPath("//input[@name= 'q']")).SendKeys(word);
+            driver.FindElement(By.XPath("//input[@name= 'q']")).SendKeys(Keys.Enter);        }
+    }
+}
